b3404059cff785e6027f6914f46df8b344802e02
TonySimonovsky <metton@gmail.com>
2024-12-17 17:28:43 +0800

    update to be able to run with a specific client

--- main.py
+++ main.py
@@ -26,19 +26,24 @@ enable_logging(logging_enabled)
 
 async def main():
     character_name_id = os.getenv("CHARACTER_NAME_ID")
-
-    sia = Sia(
-        character_json_filepath=f"characters/{character_name_id}.json",
-        twitter_creds = {
+    
+    client_creds = {}
+    if os.getenv("TW_API_KEY"):
+        client_creds["twitter_creds"] = {
             "api_key": os.getenv("TW_API_KEY"),
             "api_secret_key": os.getenv("TW_API_KEY_SECRET"),
             "access_token": os.getenv("TW_ACCESS_TOKEN"),
             "access_token_secret": os.getenv("TW_ACCESS_TOKEN_SECRET"),
             "bearer_token": os.getenv("TW_BEARER_TOKEN")
-        },
-        telegram_creds = {
+        }
+    if os.getenv("TG_BOT_TOKEN"):
+        client_creds["telegram_creds"] = {
             "tg_bot_token": os.getenv("TG_BOT_TOKEN"),
-        },
+        }
+    
+    sia = Sia(
+        character_json_filepath=f"characters/{character_name_id}.json",
+        **client_creds,
         memory_db_path=os.getenv("DB_PATH"),
         # knowledge_module_classes=[GoogleNewsModule],
         logging_enabled=logging_enabled
